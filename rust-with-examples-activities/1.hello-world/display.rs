44eeb6f1e6992fb52e349561864916eb61ec0cfe
matchy233 <matchy233@gmail.com>
2021-08-06 18:46:14 +0900

    Change file structure

---
+++ rust-with-examples-activities/1.hello-world/display.rs
@@ -0,0 +1,26 @@
+/*
+Activity
+After checking the output of the above example, use the Point2D struct as a guide to add a Complex struct to the example. When printed in the same way, the output should be:
+Display: 3.3 + 7.2i
+Debug: Complex { real: 3.3, imag: 7.2 }
+*/
+
+use std::fmt;
+
+#[derive(Debug)]
+struct Complex {
+    real: f32,
+    imag: f32
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{real} + {imag}i", real=self.real, imag=self.imag) // no semicolon returns fmt result?
+    }
+}
+
+fn main() {
+    let complex = Complex { real: 3.3, imag: 7.2 };
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
+}
